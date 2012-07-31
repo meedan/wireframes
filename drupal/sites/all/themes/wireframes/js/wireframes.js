4f3dba9168330ee73d015e3a14a19b29acca7bd3
James Andres <james.andres@gmail.com>
2012-07-31 16:08:57 -0400

    Fixing wireframes node page, adding lightbox.

---
+++ drupal/sites/all/themes/wireframes/js/wireframes.js
@@ -0,0 +1,29 @@
+(function ($) {
+
+Drupal.behaviors.wireframesAssets = {
+  attach: function (context) {
+    var $assets     = $('body.node-type-wireframe .field-name-field-asset', context),
+        $fieldItems = $('.field-items', $assets),
+        highestItem = 0,
+        fullWidth   = 0
+        padding     = parseInt($fieldItems.css('padding-top'), 10)
+                    + parseInt($fieldItems.css('padding-bottom'), 10);
+
+    $('.field-item', $assets).each(function () {
+      var $this  = $(this),
+          width  = $this.outerWidth(true),
+          height = $this.outerHeight(true);
+
+      highestItem = height > highestItem ? height : highestItem;
+      fullWidth  += width;
+    });
+
+    // Add extra height to account for box padding
+    highestItem += padding;
+
+    $assets.css({ height: highestItem });
+    $fieldItems.css({ width: fullWidth });
+  }
+};
+
+})(jQuery);
